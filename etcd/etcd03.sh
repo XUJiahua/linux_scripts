f88cad0590037874c4dd2c090c449b297d93ef46
许嘉华 <jiahua.xu@outlook.com>
2020-04-20 15:49:37 +0800

    Create etcd03.sh

---
+++ etcd/etcd03.sh
@@ -0,0 +1,19 @@
+TOKEN=token-01
+CLUSTER_STATE=new
+NAME_1=etcd01
+NAME_2=etcd02
+NAME_3=etcd03
+HOST_1=192.168.99.100
+HOST_2=192.168.99.101
+HOST_3=192.168.99.102
+CLUSTER=${NAME_1}=http://${HOST_1}:2380,${NAME_2}=http://${HOST_2}:2380,${NAME_3}=http://${HOST_3}:2380
+
+# For machine 3
+THIS_NAME=${NAME_3}
+THIS_IP=${HOST_3}
+etcd --data-dir=data.etcd --name ${THIS_NAME} \
+	--initial-advertise-peer-urls http://${THIS_IP}:2380 --listen-peer-urls http://${THIS_IP}:2380 \
+	--advertise-client-urls http://${THIS_IP}:2379 --listen-client-urls http://${THIS_IP}:2379 \
+	--initial-cluster ${CLUSTER} \
+	--initial-cluster-state ${CLUSTER_STATE} --initial-cluster-token ${TOKEN}
+  
